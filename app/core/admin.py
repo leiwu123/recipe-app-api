a75081adf835793ab4581c02c928c408507fe8b0
leiwu123 <jobspecific@hotmail.com>
2019-03-20 09:46:13 +0800

    updated admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
+# _ making the strings human readable
 
 from core import models
 
@@ -16,6 +17,12 @@ class UserAdmin(BaseUserAdmin):
       {'fields': ('is_active', 'is_staff', 'is_superuser')}
     ),
     (_('Important dates'), {'fields': ('last_login',)})
+  )
+  add_fieldsets = (
+    (None, {
+      'classes': ('wide',), # the class applied to the form
+      'fields': ('email', 'password1', 'password2')
+    }),
   )
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -33,4 +33,11 @@ class adminSiteTests(TestCase):
     res = self.client.get(url)
 
     self.assertEqual(res.status_code, 200)
+
+  def test_create_user_page(self):
+    """Test that the create user page works"""
+    url = reverse('admin:core_user_add')
+    res = self.client.get(url)
+
+    self.assertEqual(res.status_code, 200)
     
