287f1e05fdd5e64aabe04f68d3fb8379bd907a39
leiwu123 <jobspecific@hotmail.com>
2019-04-06 17:23:58 +0800

    added working list tags endpoint

--- app/core/admin.py
+++ app/core/admin.py
@@ -27,3 +27,5 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
+
--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
   PermissionsMixin
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
 
@@ -35,3 +37,14 @@ class User(AbstractBaseUser, PermissionsMixin):
   objects = UserManager()
 
   USERNAME_FIELD = 'email'  # by default username field is username
+
+class Tag(models.Model):
+  """Tag to be used for a recipe"""
+  name = models.CharField(max_length=255)
+  user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,  # django best pracice instead of just use - User
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return self.name
